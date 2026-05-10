dccff6e8ddadcc46c78badfeef76931ae2a09346
+ <+>
2026-05-10 15:04:42 -0400

    complete array filter method lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution here
+function findMatching(drivers, name) {
+  const result = drivers.filter((driver) => {
+    return driver.toLowerCase() === name.toLowerCase();
+  });
+
+  if (result.length > 0) {
+    return result;
+  } else {
+    return [];
+  }
+}
+
+function fuzzyMatch(drivers, name) {
+  return drivers.filter((driver) => {
+    return driver.toLowerCase().indexOf(name.toLowerCase()) === 0
+  })
+}
+
+function matchName(drivers, name) {
+    return drivers.filter((driver) => {
+        return driver.name === name
+    })
+}
